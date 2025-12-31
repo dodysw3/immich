2326ad3f40a3731a9615816d07f4bb06a7ab286e
Dody Suria Wijaya <dodysw@gmail.com>
2026-01-01 02:05:36 +0700

    Person name label was too small

--- web/src/lib/components/asset-viewer/face-bounding-box.svelte
+++ web/src/lib/components/asset-viewer/face-bounding-box.svelte
@@ -20,7 +20,7 @@
   <!-- Person name label (if available) -->
   {#if faceBox.personName}
     <div
-      class="absolute left-0 right-0 text-center text-white text-[7px] bg-black/75 px-1 py-0.5 rounded-b break-all max-w-full pointer-events-none"
+      class="absolute left-0 right-0 text-center text-white text-xs bg-black/75 px-1 py-0.5 rounded-b break-all max-w-full pointer-events-none"
       style="top: {faceBox.height}px;"
     >
       {faceBox.personName}
